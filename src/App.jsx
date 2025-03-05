2a765907c5ba5c3ab5a814fa84745ae487839727
felyRico <emanresutresni@gmail.com>
2025-03-05 14:42:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Slideshow from "./Slideshow";
+
+function App() {
+  return (
+    <div className="App">
+      <Slideshow />
+    </div>
+  );
+}
+
+export default App;
